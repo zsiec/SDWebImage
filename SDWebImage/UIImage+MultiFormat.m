381872cb062d4af3eb69057b60f9ebed5559f8e3
Thomas Symborski <thomas.symborski@gmail.com>
2014-12-29 15:30:25 -0500

    Remove WEBP and animated gif support for testing

--- SDWebImage/UIImage+MultiFormat.m
+++ SDWebImage/UIImage+MultiFormat.m
@@ -17,23 +17,7 @@
 @implementation UIImage (MultiFormat)
 
 + (UIImage *)sd_imageWithData:(NSData *)data {
-    UIImage *image;
-    NSString *imageContentType = [NSData contentTypeForImageData:data];
-    if ([imageContentType isEqualToString:@"image/gif"]) {
-        image = [UIImage sd_animatedGIFWithData:data];
-    }
-#ifdef SD_WEBP
-    else if ([imageContentType isEqualToString:@"image/webp"])
-    {
-        image = [UIImage sd_imageWithWebPData:data];
-    }
-#endif
-    else {
-        image = [[UIImage alloc] initWithData:data];
-    }
-
-
-    return image;
+    return [[UIImage alloc] initWithData:data];
 }
 
 @end
